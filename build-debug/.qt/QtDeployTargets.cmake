20449c3bbe3e6cb81e8ea61cad4aa92282a9b152
Calvin Chau <calvin71203@gmail.com>
2024-12-31 23:03:44 -0500

    Delete build-debug directory

--- build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,4 +0,0 @@
-set(__QT_DEPLOY_TARGET_Client_FILE /Users/calvin/Cal-Chat/build-debug/Client/Client)
-set(__QT_DEPLOY_TARGET_Client_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Server_FILE /Users/calvin/Cal-Chat/build-debug/Server/Server)
-set(__QT_DEPLOY_TARGET_Server_TYPE EXECUTABLE)
